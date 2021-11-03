f708f6028459315f368151f7bb07198eb8d92f9a
brittneypostma <brittney.postma@hotmail.com>
2021-11-02 21:52:17 -0400

    init

---
+++ svelte.config.js
@@ -0,0 +1,35 @@
+/** @type {import('@sveltejs/kit').Config} */
+import adapter from '@sveltejs/adapter-static';
+import preprocess from 'svelte-preprocess';
+import path from 'path';
+
+const config = {
+	kit: {
+		// hydrate the <div id="svelte"> element in src/app.html
+		adapter: adapter({
+			pages: 'build',
+			assets: 'build',
+			fallback: null
+		}),
+
+		target: '#svelte',
+
+		vite: {
+			resolve: {
+				alias: {
+					// these are the aliases and paths to them
+					'@components': path.resolve('./src/lib/components'),
+					'@layout': path.resolve('./src/lib/layout'),
+					'@lib': path.resolve('./src/lib'),
+					'@utils': path.resolve('./src/lib/utils'),
+					'@styles': path.resolve('./src/lib/styles'),
+					'@stores': path.resolve('./src/lib/stores')
+				}
+			}
+		}
+	},
+
+	preprocess: preprocess()
+};
+
+export default config;
