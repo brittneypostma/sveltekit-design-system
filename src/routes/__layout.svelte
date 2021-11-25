c56a54842c4cbfe8a11468b109a391cb2b62b178
brittneypostma <brittney.postma@hotmail.com>
2021-11-25 16:57:03 -0500

    styles

---
+++ src/routes/__layout.svelte
@@ -0,0 +1,5 @@
+<script>
+	import '@styles/styles.css';
+</script>
+
+<slot />
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,61 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<section style="margin-bottom: var(--cds-layout-04)">
+	<section>
+		<h1>Vanilla CSS</h1>
+		<p>
+			Vanilla CSS is a great way to create your design system. It is simple and easy to get started
+			and requires little effort to customize and add to.
+		</p>
+
+		<p>
+			Design systems facilitate design and development through reuse, consistency, and
+			extensibility.
+		</p>
+	</section>
+</section>
+<section style="margin-bottom: var(--cds-layout-02)">
+	<section>
+		<h2>Buttons</h2>
+		<hr />
+		<section>
+			<section>
+				<h3>Primary</h3>
+				<button>Primary button</button>
+			</section>
+			<section>
+				<h3>Secondary</h3>
+				<!-- <Button kind="secondary">Secondary button</Button> -->
+			</section>
+			<section>
+				<h3>Tertiary</h3>
+				<!-- <Button kind="tertiary">Tertiary button</Button> -->
+			</section>
+		</section>
+	</section>
+</section>
+<section style="margin-bottom: var(--cds-layout-02)">
+	<section>
+		<h2>Inputs</h2>
+		<hr />
+		<section>
+			<section>
+				<h3>Default</h3>
+				<label>
+					User name
+					<input type="text" placeholder="Enter user name..." />
+				</label>
+			</section>
+			<section>
+				<h3>With helper text</h3>
+				<label>
+					User name
+					<input type="text" placeholder="Enter user name..." />
+					<span class="bx--form__helper-text">Your user name is associated with your email</span>
+				</label>
+			</section>
+			<section>
+				<h3>Hidden Label</h3>
+				<input type="text" placeholder="Enter user name..." />
+			</section>
+		</section>
+	</section>
+</section>
